15753b045c8992e5b5b8e47ad371259949f6764d
Ben Franzi <ben.franzi@growsuper.com>
2023-01-31 00:08:12 +1100

    chore: setup next

--- src/utils/get-settings.js
+++
@@ -1,5 +0,0 @@
-import settings from 'config/settings/index.json';
-
-export default function getSettings() {
-  return settings;
-}
--- src/utils/theme.js
+++
@@ -1,5 +0,0 @@
-export const SCREEN_WIDTH = {
-  mobile: 480,
-  tablet: 1024,
-  desktop: 1280,
-}
