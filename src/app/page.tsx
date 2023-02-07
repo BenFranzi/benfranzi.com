c307d0553f39e30a303541ec53776ca841f153ae
Ben Franzi <ben.franzi@growsuper.com>
2023-02-08 10:29:18 +1100

    chore: force dark mode
    - reduce cache time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import SongShowcase from "@/components/song-showcase";
 import getRandomTrack from "@/utils/get-random-track";
 
-
+export const revalidate = 21600;
 
 export default async function Home() {
     const data = getRandomTrack();
