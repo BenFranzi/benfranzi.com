3504d096fa8b0c060dcfcf7a1a7f9db53e82293f
Ben Franzi <benfranzi@outlook.com>
2023-04-03 07:03:42 +0000

    feature: setup mdx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 import SongShowcase from "@/components/song-showcase";
 import getRandomTrack from "@/utils/get-random-track";
 
-export const revalidate = 21600;
-
 export default async function Home() {
-    const data = getRandomTrack();
-
     return (
-        <SongShowcase {...data} />
+        <div>Very much a work in progress...</div>
     )
 }
